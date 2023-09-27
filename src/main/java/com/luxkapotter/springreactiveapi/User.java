31981fa50e3d3d37e79597fe2e61384af649573f
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-09-26 23:01:27 -0300

    feat: creating model

---
+++ src/main/java/com/luxkapotter/springreactiveapi/User.java
@@ -0,0 +1,11 @@
+package com.luxkapotter.springreactiveapi;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("users")
+public record User(
+    @Id Long id, String username, String password, String email
+) {
+    
+}
